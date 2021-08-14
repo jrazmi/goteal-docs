a59ce9c2a97bfc0f0130b72ff88f16812eedd271
jrazmi <j.lawrence.mitchell@gmail.com>
2021-08-14 16:15:38 -0700

    cleaning next - trying create react

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-import { Layout } from '../components/Global/Layout'
-export default function About() {
-  return (
-    <Layout>
-       <h1> About us</h1>
-    </Layout>
-  )
-}
