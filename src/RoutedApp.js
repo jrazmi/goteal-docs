c5df489b353f43c668df4a541b30ed4203addc21
jrazmi <j.lawrence.mitchell@gmail.com>
2021-08-14 17:50:56 -0700

    toggle theme/styled-components

--- src/RoutedApp.js
+++ src/RoutedApp.js
@@ -1,8 +1,11 @@
 import { Route, Switch } from "react-router-dom";
-import * as Pages from './pages';
-import { Layout } from './components/Layout';
+import { ThemeProvider } from 'styled-components';
 import { AnimatePresence } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
+
+import * as Pages from './pages';
+import { Layout, GlobalStyle, DarkTheme, LightTheme } from './components/globals';
+import { useDarkMode } from "./components/hooks";
 
 const RouteMap = [
     {   
@@ -13,20 +16,36 @@ const RouteMap = [
 
 export const RoutedApp = () => {
     const location = useLocation();
+    const [theme, toggleTheme, componentMounted] = useDarkMode();
+
+    const themeMode = theme === 'light' ? LightTheme : DarkTheme;
+  
+
+    if (!componentMounted) {
+      return <div />
+    };
+
+   
+  
     return(
+        <ThemeProvider theme={themeMode}>
+            <>
+            <GlobalStyle />
             <AnimatePresence exitBeforeEnter initial={false}>
-        <Layout>
-            <Switch location={location} key={location.pathname}>
-                <Route exact path="/">
-                    <Pages.Home />
-                </Route>
-                {
-                    RouteMap.map((route) => (
-                        <Route path={route.path} component={route.component} />
-                        ))
-                    }   
-            </Switch>
-        </Layout>
-    </AnimatePresence>
+                <Layout toggleTheme={toggleTheme}>
+                    <Switch location={location} key={location.pathname}>
+                        <Route exact path="/">
+                            <Pages.Home />
+                        </Route>
+                        {
+                            RouteMap.map((route, idx) => (
+                                <Route key={idx} path={route.path} component={route.component} />
+                                ))
+                            }   
+                    </Switch>
+                </Layout>
+            </AnimatePresence>
+            </>
+        </ThemeProvider>
     )
 }
